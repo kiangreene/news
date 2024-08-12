378e62cb70ffc40c5583e846afb30e129e52eac1
kiangreene <56272993+kiangreene@users.noreply.github.com>
2024-08-12 14:54:24 -0700

    Initial commit

---
+++ api.R
@@ -0,0 +1,14 @@
+library(httr2)
+library(jsonlite)
+req <- request("https://newsapi.org/v2/everything") |>
+  req_url_query(
+    q = '`"data science"`',
+    from = Sys.Date() - 1,
+    pageSize = 10,
+    apiKey = Sys.getenv("NEWS_API_KEY")
+  )
+
+resp <- req_perform(req)
+resp_body_json(resp)
+
+jsonlite::write_json(resp_body_json(resp), path = file.path(getwd(), "data", paste0(lubridate::today(),".json")))
